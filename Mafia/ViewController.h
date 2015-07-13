347cfaf5e57cf7753cbe63f72444f8d29da25a33
Auston <auston.salvana@live.com>
2015-07-13 15:47:02 -0700

    Initial Commit

---
+++ Mafia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mafia
+//
+//  Created by Auston Salvana on 7/13/15.
+//  Copyright (c) 2015 ASolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
